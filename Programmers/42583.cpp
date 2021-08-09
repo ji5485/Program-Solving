44c9ac4e33a0e890e2a6933dc0e5974ba58fc7b2
주현도 <dong5485@gmail.com>
2021-08-09 10:58:47 +0000

    2021.08.09. Programmers Problem

---
+++ Programmers/42583.cpp
@@ -0,0 +1,37 @@
+#include <string>
+#include <vector>
+#include <queue>
+
+using namespace std;
+
+int solution(int bridge_length, int weight, vector<int> truck_weights) {
+    int answer = 0, index = 0;
+    queue<pair<int, int>> bridges;
+    
+    while (true) {
+        if (index == truck_weights.size() && bridges.empty()) break;
+      
+        if (!bridges.empty() && bridges.front().first == answer - bridge_length) {
+            weight += bridges.front().second;
+            bridges.pop();
+        }
+        
+        if (index < truck_weights.size() && weight >= truck_weights[index]) {
+            weight -= truck_weights[index];
+            bridges.push(make_pair(answer, truck_weights[index]));
+            printf("%d %d \n", answer, truck_weights[index]);
+            index++;
+        }
+        
+        answer++;
+    }
+    
+    return answer;
+}
+
+int main() {
+  vector<int> vec = { 7, 4, 5, 6 };
+  int result = solution(2, 10, vec);
+  printf("%d \n", result);
+  return 0;
+}
