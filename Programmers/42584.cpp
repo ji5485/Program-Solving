ad8e536c4c524e09e440723296fb16d6d11e71cf
주현도 <dong5485@gmail.com>
2021-08-10 10:23:49 +0000

    2021.08.10. Programmers Problem

---
+++ Programmers/42584.cpp
@@ -0,0 +1,22 @@
+#include <string>
+#include <vector>
+#include <algorithm>
+#include <queue>
+using namespace std;
+
+vector<int> solution(vector<int> prices) {
+    vector<int> answer;
+    
+    for (int i = 0; i < prices.size(); i++) {
+        int sec = 0;
+        
+        for (int j = i + 1; j < prices.size(); j++) {
+            sec++;
+            if (prices[i] > prices[j]) break;
+        }
+        
+        answer.push_back(sec);
+    }
+    
+    return answer;
+}
